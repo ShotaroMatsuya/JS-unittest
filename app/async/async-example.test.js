30c863f861bd3bf060a46ba8e979902abdeb981e
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-06-07 14:46:48 +0900

    test async code with callbacks

--- app/async/async-example.test.js
+++ app/async/async-example.test.js
@@ -1,10 +1,18 @@
 import { it, expect } from 'vitest';
 import { generateToken } from './async-example';
 
-it('should generate a token value', () => {
+it('should generate a token value', done => {
   const testUserEmail = 'test@test.com';
 
   generateToken(testUserEmail, (err, token) => {
-    expect(token).toBeDefined(2);
+    // expect(token).toBeDefined();
+
+    try {
+      expect(token).toBeDefined();
+      // expect(token).toBe(2);
+      done();
+    } catch (err) {
+      done(err);
+    }
   });
 });
