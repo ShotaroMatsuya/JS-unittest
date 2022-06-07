64b0d4224ef9ad1d0e8ff56052c56ad309bc1f57
ShotaroMatsuya <68782833+ShotaroMatsuya@users.noreply.github.com>
2022-06-07 10:59:09 +0900

    sophisticated arrange phase

---
+++ app/src/math.test.js
@@ -0,0 +1,15 @@
+import { it, expect } from 'vitest';
+import { add } from './math';
+
+it('should summarize all number values in an array', () => {
+  // Arrange
+  const numbers = [1, 2, 3];
+
+  // Act
+  const result = add(numbers);
+  const expectedResult = numbers.reduce((prevValue, curValue) => {
+    return prevValue + curValue;
+  }, 0);
+  // Assert
+  expect(result).toBe(expectedResult);
+});
